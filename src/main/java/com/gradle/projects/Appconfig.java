f7f42ad746a1b2579015b3e51a7aae396b037f47
Shrivardhan <chshrivardhan@gmail.com>
2026-01-09 21:33:41 +0530

    Simple IOC and Dependency Injection

---
+++ src/main/java/com/gradle/projects/Appconfig.java
@@ -0,0 +1,11 @@
+package com.gradle.projects;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gradle.projects")
+public class Appconfig {
+
+}
