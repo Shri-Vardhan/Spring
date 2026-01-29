6b5f54246600439a69649cc498bbd533cfb5d684
Shri Vardhan <chshrivardhan@gmail.com>
2026-01-29 20:23:44 +0530

    Add comment to Appconfig class
    Added a comment to indicate that this is a configuration class.

--- src/main/java/com/gradle/projects/Appconfig.java
+++ src/main/java/com/gradle/projects/Appconfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+  //config class
 @ComponentScan("com.gradle.projects")
 public class Appconfig {
 
